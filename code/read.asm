4f4237c528d639042dd0d7a0dccb09accadb07a3
Penetratingshot <shadowtemplates@gmail.com>
2019-05-11 20:20:46 -0700

    trying to open file from buffer

--- code/read.asm
+++ code/read.asm
@@ -4,10 +4,12 @@
 ; Hold the logic for reading from files
 section .bss
     fileName: resb 20
+    text: resb 100
 
 section .rodata
     promptFileName db "Filename: "
     promptFileNameLen equ $ - promptFileName
+    fileNameDB db "./code/text.txt", 0
 
 _readFile:
     mov rax, 1
@@ -20,6 +22,32 @@ _readFile:
     mov rdi, 0
     mov rsi, fileName
     mov rdx, 20
+    syscall
+
+    ; open the file
+    mov rax, 2
+    mov rdi, fileName
+    mov rsi, 0 ;readonly
+    mov rdx, 0644o ;file perms
+    syscall
+
+    ;read from the file
+    push rax
+    mov rdi, rax
+    mov rax, 0
+    mov rsi, text ;this is temporary, just to make sure that the read works. malloc will be used in the future
+    mov rdx, 100
+    syscall
+
+    ; close the file
+    mov rax, 3
+    pop rdi
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 100
     syscall
 
     ret
